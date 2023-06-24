5662682506cac81ac9f771d9de4f2faf4dd574c0
Alexandr-ace <syakubin@bk.ru>
2023-06-25 00:22:27 +0300

    Всё и сразу

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+let pizza: string = "Salami";
+
+let price: number = 20;
+let atStock: boolean = true;
+
+let pizzas: string[] = ["Salami", "Saussage"];
 
 function App() {
-  return (
-    <div className="App">
-      New App!awdawdawdwad
-    </div>
-  );
+  return <div className="App">{pizzas}</div>;
 }
 
 export default App;
